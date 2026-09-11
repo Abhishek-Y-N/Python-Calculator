dab434f2e21e7e96fdc8cf22f3646d992746f20d
Abhishek-Y-N <abhisheknisargandh358@gamil.com>
2026-09-11 13:37:59 +0530

    New Files

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,19 @@
-num1=float(input("Enter Frst Number:"))
+num1=float(input("Enter First Number:"))
 num2=float(input("Enter Second Number:"))
 
 op=input("Enter operation(+, -, /, *): ")
 
-if(op =="+"):
-	print("Addition:",num1+num2)
-elif(op=="-"):
-	print("Subtraction:",num1-num2)
-elif(op=="*"):
-	print("Multiplication:",num1*num2)
-elif(op=="/"):
-	if(num2==0):
-		print("Cannot Divide by 0")
-	print("Divion:",num1/num2)	
-else:
-	print("Invalid Choice")
+match op:
+	case "+":
+		print("Addition:",num1+num2)
+	case "-":
+		print("Subtraction:",num1-num2)
+	case "*":
+		print("Multiplication:",num1*num2)
+	case "/":
+		if(num2==0):``
+			print("Cannot Divide By Zero")
+		else:
+			print("Division:",num1/num2)
+	case _:
+		print("Invaid Choice")
