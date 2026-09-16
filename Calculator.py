660d4cacefb0770de5d19d795601a22e25955e6c
Abhishek-Y-N <abhisheknisargandh358@gamil.com>
2026-09-16 18:43:57 +0530

    Added Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,38 @@
-num1=float(input("Enter First Number:"))
-num2=float(input("Enter Second Number:"))
+while True:
+	print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Factorial\nType 'exit' To Terminate")
+	ch=input("Enter Your Choice:")
 
-op=input("Enter operation(+, -, /, *): ")
-
-match op:
-	case "+":
-		print("Addition:",num1+num2)
-	case "-":
-		print("Subtraction:",num1-num2)
-	case "*":
-		print("Multiplication:",num1*num2)
-	case "/":
-		if(num2==0):            			
-			print("Cannot Divide By Zero")
-		else:
-			print("Division:",num1/num2)
-	case _:
-		print("Invaid Choice")
+	match ch:
+		case "1":
+			num1=float(input("Enter 1st Number:"))
+			num2=float(input("Enter 2nd Number:"))
+			print("Addition:",num1+num2)
+		case "2":
+			num1=float(input("Enter 1st Number:"))
+			num2=float(input("Enter 2nd Number:"))
+			print("Subtraction:",num1-num2)
+		case "3":
+			num1=float(input("Enter 1st Number:"))
+			num2=float(input("Enter 2nd Number:"))
+			print("Multiplication:",num1*num2)
+		case "4":
+			num1=float(input("Enter 1st Number:"))
+			num2=float(input("Enter 2nd Number:"))
+			if(num2==0):
+				print("Cannot Divide By Zero!")
+			else:
+				print("Division:",num1/num2)
+		case "5":
+			num=float(input("Enter A Number:"))
+			i=1
+			fact=1
+			while i<=num:
+				fact=fact*i      
+				i+=1
+			print("Factorial:",fact)
+		case "exit":
+			print("Terminated")
+			break
+		case _:
+			print("Invalid Choice")
+	 
